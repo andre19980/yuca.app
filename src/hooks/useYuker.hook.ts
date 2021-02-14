c3c2c77b79a23b1524ced2bf9b74f10108b1a17e
André Solak <andre.solak@basement.io>
2021-02-14 11:35:54 -0300

    Update yuker hook

--- src/hooks/useYuker.hook.ts
+++ src/hooks/useYuker.hook.ts
@@ -1,16 +1,42 @@
 import { useState, useCallback } from 'react';
 import axios from 'axios';
 
+import { YuckerState } from '../types/yuker.type';
+import { UserApi } from '../models/user/user.model';
+import { Apartment } from '../models/apartment';
+import { ApartmentApi } from '../models/apartment/apartment.model';
+import { ServiceApi } from '../models/service/service.model';
+
+interface IndexResponse {
+  user: UserApi;
+  apartment: ApartmentApi;
+  services: ServiceApi[];
+}
+
 const useYuker = () => {
-  const [yuker, setYuker] = useState();
+  const [yuker, setYuker] = useState({} as YuckerState);
 
   const getYuker = useCallback(async () => {
-    const response = await axios.get('https://private-42e99d-yuca1.apiary-mock.com/me');
+    try {
+      await axios
+        .get<IndexResponse>('https://private-42e99d-yuca1.apiary-mock.com/me')
+        .then(response => {
+          const { user, apartment, services } = response.data;
+          setYuker({
+            user,
+            apartment,
+            services,
+          })
+        });
 
-    console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+
   }, []);
 
   return {
+    yuker,
     getYuker
   };
 }
